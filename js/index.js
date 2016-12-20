c50c8fd556a06c0a59e3ef127dd3379dfc6a9462
garronej <joseph.garrone.gj@gmail.com>
2016-12-20 15:59:25 +0000

    Target es3

--- js/index.js
+++ js/index.js
@@ -1,47 +1,57 @@
 "use strict";
 function i1o1(asyncFunction) {
-    return (input) => {
-        return new Promise(resolve => {
-            asyncFunction(input, (output) => resolve(output));
+    return function (input) {
+        return new Promise(function (resolve) {
+            asyncFunction(input, function (output) { return resolve(output); });
         });
     };
 }
 exports.i1o1 = i1o1;
 ;
 function i1o2(asyncFunction) {
-    return (input) => {
-        return new Promise(resolve => {
-            asyncFunction(input, (output1, output2) => resolve([output1, output2]));
+    return function (input) {
+        return new Promise(function (resolve) {
+            asyncFunction(input, function (output1, output2) { return resolve([output1, output2]); });
         });
     };
 }
 exports.i1o2 = i1o2;
 ;
 function i2o1(asyncFunction) {
-    return (input1, input2) => {
-        return new Promise(resolve => {
-            asyncFunction(input1, input2, (output) => resolve(output));
+    return function (input1, input2) {
+        return new Promise(function (resolve) {
+            asyncFunction(input1, input2, function (output) { return resolve(output); });
         });
     };
 }
 exports.i2o1 = i2o1;
 ;
 function i2o2(asyncFunction) {
-    return (input1, input2) => {
-        return new Promise(resolve => {
-            asyncFunction(input1, input2, (output1, output2) => resolve([output1, output2]));
+    return function (input1, input2) {
+        return new Promise(function (resolve) {
+            asyncFunction(input1, input2, function (output1, output2) { return resolve([output1, output2]); });
         });
     };
 }
 exports.i2o2 = i2o2;
 ;
 function promisify(asyncFunction) {
-    return (...inputs) => {
-        return new Promise(resolve => {
-            asyncFunction.apply(null, inputs.concat([(...outputs) => resolve(outputs)]));
+    return function () {
+        var inputs = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            inputs[_i] = arguments[_i];
+        }
+        return new Promise(function (resolve) {
+            asyncFunction.apply(null, inputs.concat([function () {
+                    var outputs = [];
+                    for (var _i = 0; _i < arguments.length; _i++) {
+                        outputs[_i] = arguments[_i];
+                    }
+                    return resolve(outputs);
+                }]));
         });
     };
 }
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = promisify;
+exports.__esModule = true;
+exports["default"] = promisify;
 ;
